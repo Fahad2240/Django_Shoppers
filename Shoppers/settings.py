b31cb9cff41d91d7ed72e7db2ebf76e1a655b7a0
Fahad2240 <fahadish861@gmail.com>
2023-11-17 12:50:03 +0600

    update

--- Shoppers/settings.py
+++ Shoppers/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 LOGIN_URL='/login/'
-LOGIN_REDITERCT_URL='/'
+LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 
 # Application definition
--- make/urls.py
+++ make/urls.py
@@ -10,5 +10,6 @@ urlpatterns=[
     path('signup/',views.signup,name='signup'),
     path('login/', auth_view.LoginView.as_view(template_name='login.html',
          authentication_form=LoginForm), name='login'),
+    path('logout/',auth_view.LogoutView.as_view(),name='logout'),
     path('contact/',views.contact,name='contact')
 ]
